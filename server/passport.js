9e000ebc6e9cf9e4b280d5d33b2e5c599a2e6dde
choosak kwanraksri <btk1392545@gmail.com>
2023-12-24 16:47:02 +0700

    set proxy

--- server/passport.js
+++ server/passport.js
@@ -12,6 +12,7 @@ passport.use(
             clientID: GOOGLE_CLIENT_ID,
             clientSecret: GOOGLE_CLIENT_SECRET,
             callbackURL: "/auth/google/callback",
+            proxy: true
         },
         async function (accessToken, refreshToken, profile, done) {
 
